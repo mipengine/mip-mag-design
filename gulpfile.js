ce814416f924e8e5b5a5e9ab2022d81aa7d5b3c0
dongshihao <dshhit@gmail.com>
2017-11-08 17:34:22 +0800

    browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,19 @@
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
-const cleanCSS = require('gulp-clean-css');
 const rimraf = require('gulp-rimraf');
 const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat'); 
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync');
 
 /*************************************************************************************************************************************/
 
-let cleanCSSOptions = function () {
-    return cleanCSS({
-        format: {
-            breaks: {
-                afterAtRule: true,
-                afterBlockBegins: true,
-                afterBlockEnds: true,
-                afterComment: true,
-                afterProperty: true,
-                afterRuleBegins: true,
-                afterRuleEnds: true,
-                beforeBlockEnds: true,
-                betweenSelectors: true
-            },
-            indentBy: 4,
-            indentWith: 'space',
-            spaces: {
-                aroundSelectorRelation: true,
-                beforeBlockBegins: true,
-                beforeValue: true
-            },
-            wrapAt: false
-        }
-    });
+let paths = {
+	src: 'src/**/*.styl',
+	dist: 'dist',
+	distModule: 'dist/css',
+	demo: '',
+	baseDir: 'example'
 };
 
 gulp.task('clean', function () {
@@ -39,21 +22,31 @@ gulp.task('clean', function () {
 });
 
 gulp.task('build-min', ['clean'], function () {
-    return gulp.src('src/**/*.styl')
+    return gulp.src(paths.src)
         .pipe(sourcemaps.init())
         .pipe(stylus())
-        .pipe(cleanCSSOptions())
+        .pipe(cleanCSS())
         .pipe(concat('mag-design.min.css'))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(paths.dist))
+        .pipe(gulp.dest(paths.baseDir));
 });
 
 gulp.task('build-module-css', ['clean'], function () {
-    return gulp.src('src/**/*.styl')
+    return gulp.src(paths.src)
         .pipe(stylus())
-        .pipe(cleanCSSOptions())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(paths.distModule));
 });
 
 gulp.task('build', ['build-module-css', 'build-min']);
 
+
+
+gulp.task('preview', ['build'], function () {
+   browserSync.init([paths.src, paths.demo], {
+      server: {
+         baseDir: paths.baseDir
+      }
+   });
+});
