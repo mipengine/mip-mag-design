b83112ad35df4e2bb9f959bb7049c72ebc01985f
xuexb <fe.xiaowu@gmail.com>
2018-02-02 15:03:38 +0800

    feat: 添加字体导出多格式

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('iconfont', function () {
   .pipe(iconfont({
     fontName: fontName,
     prependUnicode: true,
-    formats: ['ttf', 'eot', 'woff'],
+    formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
     timestamp: runTimestamp,
     normalize: true,
     fontHeight: 1000
